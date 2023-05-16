6bd07b82cd28bd84fa08695638c3d7cc6b4ffa21
ZhuYuQin24 <zyq_6828@163.com>
2023-05-16 15:05:58 +0800

    vite中如何使用@ 配置路径别名?

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve:{
+    //vite中如何使用@ 配置路径别名?
+    alias:[{find:"@",replacement:path.resolve(__dirname,"src")}]
+  }
 })
