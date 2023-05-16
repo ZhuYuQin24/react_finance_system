affaf91b644b59d81bfb263bc7b095aeac1d970d
ZhuYuQin24 <zyq_6828@163.com>
2023-05-16 18:28:03 +0800

    init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./themes";
+import {CssBaseline } from "@mui/material";
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-     
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+      
+        </CssBaseline>
+      </ThemeProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
