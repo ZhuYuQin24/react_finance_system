eb0fbbbbeabcb4bcd320cecde4bdaa2b1d934608
ZhuYuQin24 <zyq_6828@163.com>
2023-05-23 13:28:55 +0800

    组件

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { themeSettings } from "./themes";
-import {CssBaseline } from "@mui/material";
+import {Box,CssBaseline } from "@mui/material";
+import {  Routes,Route} from "react-router-dom";
+import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
+import Predictions from "@/scenes/predictions";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -9,7 +13,13 @@ function App() {
     <div className="app">
       <ThemeProvider theme={theme}>
         <CssBaseline>
-      
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<Predictions />} />
+            </Routes>
+          </Box>
         </CssBaseline>
       </ThemeProvider>
     </div>
