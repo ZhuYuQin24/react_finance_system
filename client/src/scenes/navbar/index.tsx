ded74075b9f9bba4f03d07da9d132d1b3a6c77d1
ZhuYuQin24 <zyq_6828@163.com>
2023-05-23 14:34:48 +0800

    头部导航栏

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Box, Typography, useTheme } from "@mui/material";
+import FlexBetween from "@/components/FlexBetween";
 
 type Props = {};
 const Navbar = (props: Props) => {
   const { palette } = useTheme();
-  return <Box display="flex"
-  justifyContent="space-between"
-  alignItems="center"
-  ></Box>;
+  return <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}
+  >
+    hey
+  </FlexBetween>;
 };
 
 export default Navbar;
---
+++ client/src/scenes/predictions/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Predictions() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Predictions
