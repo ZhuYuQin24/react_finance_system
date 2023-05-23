b6467dddf7a5e5b6ecc558ca88569e01cf896466
ZhuYuQin24 <zyq_6828@163.com>
2023-05-23 17:33:29 +0800

    主题组件

--- client/src/components/Theme.tsx
+++ client/src/components/Theme.tsx
@@ -2,15 +2,12 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { themeSettings } from "@/themes";
 
-
 function Theme() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
-    <div className="app">
-      <ThemeProvider theme={theme}>
-       
-      </ThemeProvider>
-    </div>
+   
+      <ThemeProvider theme={theme}></ThemeProvider>
+  
   );
 }
 
